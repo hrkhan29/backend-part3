ebc867b0667604b0fbc958f57c29da0e837e94da
hrkhan29 <hrkhan29@hotmail.com>
2023-04-06 16:58:20 +0600

    frontend build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+app.use(express.static('build'))
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
